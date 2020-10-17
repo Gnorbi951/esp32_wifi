949e4eebfdd33c573a0025519bab31895565fe65
Gőcze Norbert <gnorbi951@gmail.com>
2020-10-17 22:23:47 +0200

    Implement connection

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include "Wifi.h"
 
-void setup() {
+#define WIFI_NETWORK "G-Home"
+#define WIFI_PASSWORD "nop"
+#define WIFI_TIMEOUT_MS 20000
+
+void connectToWifi(){
+    Serial.print("Connecting to Wifi");
+    Wifi.mode(WIFI_STA);
+    Wifi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
+    unsigned long startAttemptTime = milis();
+
+    while(Wifi.status() != WL_CONNECTED && milis() - startAttemptNow < WIFI_TIMEOUT){
+        Serial.print(".");
+        delay(100);
+    }
+}
+
+void setup(){
 
 }
 
-void loop() {
-    
+void loop(){
+
 }
